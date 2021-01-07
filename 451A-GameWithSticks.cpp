523229a26c8e7422fb784dfe892d31ec9a264725
Samiksha Modi <55681233+samikshamodi@users.noreply.github.com>
2021-01-07 20:03:46 +0530

    7 Jan 2021

---
+++ 451A-GameWithSticks.cpp
@@ -0,0 +1,50 @@
+/* samikshamodi - Samiksha Modi */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+
+#define sync ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define out1d(arr,size) {for(int i=0;i<size;i++) cout<<arr[i]<<" ";}
+#define in2d(arr,row,col) for(int i=0;i<row;i++) for(int j=0;j<col;j++) cin>>arr[i][j];
+#define out2d(arr,row,col) {for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cout<<arr[i][j]<<" ";cout<<endl;}}
+#define mem(arr,val) memset(arr,val,sizeof(arr));
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ll N = 1e5 + 7;
+
+int intpts(int n,int m)
+{
+	return n*m;
+}
+	
+int main()
+{
+    sync;
+    int n;
+    int m;
+    cin>>n>>m;
+
+    int cnt=0;
+    while(intpts(n,m)!=0)
+    {
+    	cnt++;
+    	n--;
+    	m--;
+    }
+
+    if(cnt%2==0)
+    	cout<<"Malvika";
+    else
+    	cout<<"Akshat";
+
+
+    return 0;
+}
