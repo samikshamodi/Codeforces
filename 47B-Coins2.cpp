02f6507840ec56167d176d1ce56ac624e2a8d68e
Samiksha Modi <55681233+samikshamodi@users.noreply.github.com>
2021-01-12 19:02:54 +0530

    12 Jan 2021

---
+++ 47B-Coins2.cpp
@@ -0,0 +1,63 @@
+/* samikshamodi - Samiksha Modi */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+
+#define sync ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define out1d(arr,size) {for(int i=0;i<size;i++) cout<<arr[i]<<" ";}
+#define in2d(arr,row,col) for(int i=0;i<row;i++) for(int j=0;j<col;j++) cin>>arr[i][j];
+#define out2d(arr,row,col) {for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cout<<arr[i][j]<<" ";cout<<endl;}}
+#define mem(arr,val) memset(arr,val,sizeof(arr));
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ll N = 1e5 + 7;
+
+int main()
+{
+    sync;
+    char x,s,y;
+    int a[3]={};
+    for(int i = 0; i < 3; i++)
+    {
+        cin >> x>>s>>y;
+        if(s== '>')
+        {
+            a[x- 'A'] += 1;
+        }
+        else
+        {
+            a[y - 'A'] += 1;
+        }
+    }
+
+
+    char ch = 'A';
+    vector<pair<int, char>> v;
+
+
+
+    if(a[0] == a[1] || a[1] == a[2] || a[2] == a[0])
+        cout << "Impossible";
+    else
+    {
+        for(int i = 0; i < 3; i += 1, ch++)
+            v.pb(mp(a[i], ch));
+            
+        sort(v.begin(), v.end());
+
+
+        for(int i = 0; i < 3; i += 1)
+            cout << v[i].second;
+
+    }
+
+    return 0;
+}
